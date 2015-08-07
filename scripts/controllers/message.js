a2399179376dba7ea2d15a57653c939b21a0929e
Spencer Davies <spencerhdavies@gmail.com>
2015-08-06 21:21:35 -0600

    finally finished the project

--- scripts/controllers/message.js
+++ scripts/controllers/message.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('chattyApp')
-  .controller('MessageCtrl', function ($scope) {
-    $scope.messages = [];
+  .controller('MessageCtrl', function ($scope, MessageService) {
+  	$scope.getMessages = function() {
+  		MessageService.getMessages().then(function(response) {
+  			$scope.messages = response.data;
+  		})
+  	}();
+
+  	$scope.addMessage = function() {
+  		MessageService.addMessage($scope.newMessage)
+  		.then(function(response) {
+  			$scope.messages = response;
+  			$scope.newMessage = "";
+  		})
+  	}
   });
--- scripts/services/messageservice.js
+++ scripts/services/messageservice.js
@@ -1,7 +1,30 @@
 'use strict';
 
 angular.module('chattyApp')
-  .factory('MessageService', function MessageService() {
-    return {
+  .service('MessageService', function MessageService($q, $http) {
+    this.getMessages = function() {
+    	var deferred = $q.defer();
+    	$http.get('http://localhost:8887')
+    	.then(function(response) {
+    		deferred.resolve(response);
+    	})
+
+    	return deferred.promise;
+    }
+
+    this.addMessage = function(newMessage) {
+      var deferred = $q.defer();
+
+      $http({
+        method: 'POST',
+        url: 'http://localhost:8887',
+        data: {
+          message: newMessage
+        }
+      }).success(function(data) {
+        deferred.resolve(data);
+      });
+
+      return deferred.promise;
     }
   });
